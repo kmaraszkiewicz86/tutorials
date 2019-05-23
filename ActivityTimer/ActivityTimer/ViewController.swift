e28b6e991862f8d81c08f2ee8d2e4d3497a883a0
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2019-05-23 23:59:27 +0200

    Add basic structure of activity timer iOS and watchOS app

---
+++ ActivityTimer/ActivityTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActivityTimer
+//
+//  Created by Krzysztof Maraszkiewicz on 23/05/2019.
+//  Copyright © 2019 Krzysztof Maraszkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
