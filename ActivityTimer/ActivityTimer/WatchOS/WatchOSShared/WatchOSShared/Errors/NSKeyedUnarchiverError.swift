533245b509db0de513e37849d94af9a6544b6956
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2019-06-13 23:24:18 +0200

    Add missing files for Activity Timer app

---
+++ ActivityTimer/ActivityTimer/WatchOS/WatchOSShared/WatchOSShared/Errors/NSKeyedUnarchiverError.swift
@@ -0,0 +1,14 @@
+//
+//  NSKeyedUnarchiver.swift
+//  ActivityTimer
+//
+//  Created by Krzysztof Maraszkiewicz on 08/06/2019.
+//  Copyright © 2019 Krzysztof Maraszkiewicz. All rights reserved.
+//
+
+///The NSKeyedUnarchiver error types
+public enum NSKeyedUnarchiverError: Error {
+    
+    ///The NSKeyedUnarchiver error type
+    case error(errorMessage: String)
+}
---
+++ ActivityTimer/ActivityTimer/WatchOS/WatchOSShared/WatchOSShared/Extensions/OSLogExtension.swift
@@ -0,0 +1,38 @@
+//
+//  OSLogExtension.swift
+//  ActivityTimer
+//
+//  Created by Krzysztof Maraszkiewicz on 25/05/2019.
+//  Copyright © 2019 Krzysztof Maraszkiewicz. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+///The OSLog extension
+public extension OSLog {
+ 
+    ///The bundle indetifier
+    private static var bundle = Bundle.main.bundleIdentifier!
+    
+    ///The ActivityTableViewController name for oslog class
+    static let activityTableViewController = OSLog(subsystem: bundle, category: "ActivityTableViewController")
+    
+    ///The ActivityService name for oslog class
+    static let activityService = OSLog(subsystem: bundle, category: "ActivityService")
+    
+    ///The ActivityInterfaceController name for oslog class
+    static let activityInterfaceController = OSLog(subsystem: bundle, category: "ActivityInterfaceController")
+    
+    ///The NSKeyedArchiverExtension name for oslog class
+    static let nsKeyedArchiverExtension = OSLog(subsystem: bundle, category: "NSKeyedArchiverExtension")
+    
+    ///The NSKeyedUnarchiverExtension name for oslog class
+    static let nsKeyedUnarchiverExtension = OSLog(subsystem: bundle, category: "NSKeyedUnarchiverExtension")
+    
+    ///The ActivityCollection name for oslog class
+    static let activityCollection = OSLog(subsystem: bundle, category: "ActivityCollection")
+    
+    static let initIOSSession = OSLog(subsystem: bundle, category: "InitIOSSession")
+    
+}
