608a5e7d7a9d53b7cc6e63b862d125b826954b17
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2019-07-30 19:27:00 +0200

    Complete a second stage of tutorial

--- AutoLayoutTutorial/AutoLayoutProgrammaticallyTutorial/AutoLayoutProgrammaticallyTutorial/ViewController.swift
+++ AutoLayoutTutorial/AutoLayoutProgrammaticallyTutorial/AutoLayoutProgrammaticallyTutorial/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let imageView: UIImageView = {
-        let imageView = UIImageView(image: #imageLiteral(resourceName: "krowa"))
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "rycka"))
         //enable autolayout for this imageView
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
@@ -23,10 +23,17 @@ class ViewController: UIViewController {
         let textView = UITextView()
         
         textView.text = "To jest testowy nagłówek!"
+        
+        let attributeText = NSMutableAttributedString(string: "To jest testowy nagłówek!", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 18)])
+        
+        attributeText.append(NSMutableAttributedString(string: "\n\n\nTo jest test testów, tej testowej aplikacji, którą tworze dla testowego wyniku", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13), NSAttributedString.Key.foregroundColor: UIColor.gray]))
+        
+        textView.attributedText = attributeText
+        
         textView.translatesAutoresizingMaskIntoConstraints = false
-        textView.font = UIFont.boldSystemFont(ofSize: 18)
         textView.textAlignment = .center
         textView.isEditable = false
+        textView.isScrollEnabled = false
         
         return textView
     }()
@@ -35,7 +42,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        self.view.addSubview(imageView)
+        //self.view.addSubview(imageView)
         self.view.addSubview(descriptionTextView)
         
         self.setupLayout()
@@ -44,24 +51,26 @@ class ViewController: UIViewController {
     private func setupLayout() {
         
         let imageViewContainerView = UIView()
-        imageViewContainerView.backgroundColor = .blue
         imageViewContainerView.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(imageViewContainerView)
         
-        imageViewContainerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        imageViewContainerView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
         imageViewContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         imageViewContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         imageViewContainerView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
         
-        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
-        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
-        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        imageViewContainerView.layoutMargins.top = 0
         
-        descriptionTextView.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20).isActive = true
-        descriptionTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        descriptionTextView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        imageViewContainerView.addSubview(imageView)
+        
+        imageView.centerXAnchor.constraint(equalTo: imageViewContainerView.centerXAnchor).isActive = true
+        imageView.centerYAnchor.constraint(equalTo: imageViewContainerView.centerYAnchor).isActive = true
+        imageView.heightAnchor.constraint(equalTo: imageViewContainerView.heightAnchor, multiplier: 0.5).isActive = true
+        
+        descriptionTextView.topAnchor.constraint(equalTo: imageViewContainerView.bottomAnchor).isActive = true
+        descriptionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24).isActive = true
+        descriptionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24).isActive = true
         descriptionTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0).isActive = true
     }
 }
