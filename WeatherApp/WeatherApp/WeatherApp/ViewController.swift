cc7e9e8fbf3dd6df4168256436503f646545a85f
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2019-12-21 21:24:05 +0100

    Change app structure from swift ui to storybord app

---
+++ WeatherApp/WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Krzysztof Maraszkiewicz on 21/12/2019.
+//  Copyright © 2019 Krzysztof Maraszkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
