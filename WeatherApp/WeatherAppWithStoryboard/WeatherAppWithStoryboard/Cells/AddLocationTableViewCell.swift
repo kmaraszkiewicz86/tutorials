bc4cfbebac3ba68930c29e37e9f1137d183d0003
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2020-01-11 13:40:31 +0100

    Updates for favorites controller

---
+++ WeatherApp/WeatherAppWithStoryboard/WeatherAppWithStoryboard/Cells/AddLocationTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  AddLocationTableViewCell.swift
+//  WeatherAppWithStoryboard
+//
+//  Created by Krzysztof Maraszkiewicz on 11/01/2020.
+//  Copyright © 2020 Krzysztof Maraszkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class AddLocationTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var addFavoritesLocation: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- WeatherApp/WeatherAppWithStoryboard/WeatherAppWithStoryboard/Controllers/FavoritesTableViewController.swift
+++ WeatherApp/WeatherAppWithStoryboard/WeatherAppWithStoryboard/Controllers/FavoritesTableViewController.swift
@@ -24,23 +24,12 @@ class FavoritesTableViewController: UITableViewController {
         super.viewDidLoad()
 
         formatter.dateFormat = "H:mm"
-        
+        loadFavorites()
         if favorites.count == 0 {
             var loc = Location.init(city: City.NewYork)
             loc.timeZone = -5 * 3600
             favorites.append(loc)
         }
-    }
-
-    private func loadFavorites() {
-        if FileManager.default.fileExists(atPath: filePath) {
-            if let file = FileHandle(forReadingAtPath: filePath) {
-                let data = file.readDataToEndOfFile()
-                let favs = try? JSONDecoder().decode([Location].self, from: data)
-                favorites = favs!
-            }
-        }
-        
     }
     
     // MARK: - Table view data source
@@ -57,63 +46,58 @@ class FavoritesTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationTableViewCell", for: indexPath) as! LocationTableViewCell
 
-        let location = favorites[indexPath.row]
-        cell.city.text = location.city.name
-        cell.temperature.text = "\(location.temperature)°"
-        let time = Date()
-        formatter.timeZone = TimeZone(secondsFromGMT: location.timeZone)
-        
-        cell.time.text = formatter.string(from: time)
+        if indexPath.row < favorites.count {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "LocationTableViewCell", for: indexPath) as! LocationTableViewCell
+            
+            let location = favorites[indexPath.row]
+            cell.city.text = location.city.name
+            cell.temperature.text = "\(location.temperature)°"
+            let time = Date()
+            formatter.timeZone = TimeZone(secondsFromGMT: location.timeZone)
+            
+            cell.time.text = formatter.string(from: time)
+            
+            return cell
+        }
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AddLocationTableViawCell", for: indexPath) as! AddLocationTableViewCell
 
         return cell
     }
     
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if indexPath.row == favorites.count {
+            //TODO: tworzenie nowego kontenera
+        } else {
+            saveFavorites()
+        }
+        
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    private func loadFavorites() {
+        if FileManager.default.fileExists(atPath: filePath) {
+            if let file = FileHandle(forReadingAtPath: filePath) {
+                let data = file.readDataToEndOfFile()
+                let favs = try? JSONDecoder().decode([Location].self, from: data)
+                favorites = favs!
+            }
+        }
+        
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    private func saveFavorites() {
+        let encoded = try? JSONEncoder().encode(favorites)
+        
+        if !FileManager.default.fileExists(atPath: filePath) {
+            FileManager.default.createFile(atPath: filePath, contents: encoded, attributes: nil)
+        } else {
+            if let file = FileHandle(forWritingAtPath: filePath) {
+                file.write(encoded!)
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
