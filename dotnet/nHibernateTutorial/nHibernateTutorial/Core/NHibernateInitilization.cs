46e2c302c11a94d8c527343450f77753fc1b7a64
kmaraszkiewicz <krzysztof.maraszkiewicz@ultrico.com>
2020-01-30 21:00:52 +0100

    Add fluent nhibernate

---
+++ dotnet/nHibernateTutorial/nHibernateTutorial/Core/NHibernateInitilization.cs
@@ -0,0 +1,28 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Dialect;
+
+namespace nHibernateTutorial.Core
+{
+    static class NHibernateInitilization
+    {
+        private static Configuration _nhConfig = new Configuration();
+        
+        public static ISessionFactory ConfigureFromConfigFile()
+        {
+            return _nhConfig.Configure().BuildSessionFactory();
+        }
+
+        public static ISessionFactory ConfgigurationFromCode()
+        {
+            _nhConfig = _nhConfig.DataBaseIntegration(db =>
+            {
+                db.Dialect<MsSql2012Dialect>();
+                db.ConnectionStringName = "db";
+                db.BatchSize = 100;
+            });
+
+            return _nhConfig.BuildSessionFactory();
+        }
+    }
+}
