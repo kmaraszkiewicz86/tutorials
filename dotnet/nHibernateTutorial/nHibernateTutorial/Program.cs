525421488707d492c436acb286fe079a41a8ab9a
kmaraszkiewicz <krzysztof.maraszkiewicz@ultrico.com>
2020-01-31 18:12:56 +0100

    Add mapping classes into db tables

--- dotnet/nHibernateTutorial/nHibernateTutorial/Program.cs
+++ dotnet/nHibernateTutorial/nHibernateTutorial/Program.cs
@@ -2,37 +2,44 @@
 using nHibernateTutorial.Core;
 using nHibernateTutorial.Enums;
 using System;
+using NHibernate.Cfg;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Tool.hbm2ddl;
+using nHibernateTutorial.Models;
 
 namespace nHibernateTutorial
 {
     class Program
     {
-        static NHibernateConfigType _type = NHibernateConfigType.fluentibeHibernate;
+        static NHibernateConfigType _type = NHibernateConfigType.appCnfig;
 
         static ISessionFactory _sessionFactory;
 
         static void Main(string[] args)
         {
             log4net.Config.XmlConfigurator.Configure();
+            var nhConfig = new Configuration().Configure();
+            var mapper = new ConventionModelMapper();
+            nhConfig.AddMapping(mapper.CompileMappingFor(new [] { typeof(TestClass) }));
 
-            switch (_type)
-            {
-                case NHibernateConfigType.cfgXml:
-                    _sessionFactory = NHibernateInitilization.ConfigureFromConfigFile();
-                    break;
+            var schemaExport = new SchemaExport(nhConfig);
+            schemaExport.Create(false, true);
 
-                case NHibernateConfigType.appCnfig:
-                    _sessionFactory = NHibernateInitilization.ConfigureFromConfigFile();
-                    break;
+            //switch (_type)
+            //{
+            //    case NHibernateConfigType.cfgXml:
+            //    case NHibernateConfigType.appCnfig:
+            //        _sessionFactory = NHibernateInitilization.ConfigureFromConfigFile();
+            //        break;
 
-                case NHibernateConfigType.code:
-                    _sessionFactory = NHibernateInitilization.ConfgigurationFromCode();
-                    break;
+            //    case NHibernateConfigType.code:
+            //        _sessionFactory = NHibernateInitilization.ConfgigurationFromCode();
+            //        break;
 
-                case NHibernateConfigType.fluentibeHibernate:
-                    _sessionFactory = FluentHibernateInitiliazation.ConfigureFromCode();
-                    break;
-            }
+            //    case NHibernateConfigType.fluentibeHibernate:
+            //        _sessionFactory = FluentHibernateInitiliazation.ConfigureFromCode();
+            //        break;
+            //}
 
             Console.WriteLine("Nhibernate configured!");
 
