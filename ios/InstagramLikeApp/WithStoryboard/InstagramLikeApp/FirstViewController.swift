640ecfad083fca04fa89df3f36d846d6058fe5d2
Krzysztof Maraszkiewicz <kmaraszkiewicz86@gmail.com>
2020-01-31 10:04:55 +0100

    Add base version of instagram base app

---
+++ ios/InstagramLikeApp/WithStoryboard/InstagramLikeApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  InstagramLikeApp
+//
+//  Created by Krzysztof Maraszkiewicz on 31/01/2020.
+//  Copyright © 2020 Krzysztof Maraszkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios/InstagramLikeApp/WithStoryboard/InstagramLikeApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  InstagramLikeApp
+//
+//  Created by Krzysztof Maraszkiewicz on 31/01/2020.
+//  Copyright © 2020 Krzysztof Maraszkiewicz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
