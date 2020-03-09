89a5bb5b168a6121d43bf721af0ddfa03795257e
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-03-10 00:02:06 +0100

    Add angular unit testing tutorial project

---
+++ angular/testing/angular-testing/src/app/services/users/users.service.spec.ts
@@ -0,0 +1,37 @@
+import { TestBed } from '@angular/core/testing';
+
+import { UsersService } from './users.service';
+
+describe('UsersService', () => {
+  let service: UsersService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UsersService]
+    });
+    service = TestBed.get(UsersService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('all', () => {
+    it('should return a collection of users', () => {
+      const userResponse = [
+        {
+          id: '1',
+          name: 'Jane',
+          role: 'Designer',
+          pokemon: 'Blastoise'
+        },
+        {
+          id: '2',
+          name: 'Bob',
+          role: 'Developer',
+          pokemon: 'Charizard'
+        }
+      ];
+    })
+  });
+});
