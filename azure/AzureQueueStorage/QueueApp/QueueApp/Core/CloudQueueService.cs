2c698212e2cc685dc27731cfb84634b5c3c5354f
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-03-10 00:01:17 +0100

    Add azure queue storage tutorail app, code refactorings

---
+++ azure/AzureQueueStorage/QueueApp/QueueApp/Core/CloudQueueService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace QueueApp.Core
+{
+    public class CloudQueueService
+    {
+        private const string QueueName = "newsqueue";
+
+        private const string ConnectionString =
+            "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=articlesubmission;AccountKey=AnGdUqQguM072jrVtJb5tNygXoU+c9dAJPywJ3czvfx+qFhKdCao9zuaBEd/1MTWSLharK6I/LqOXGSrsMrekQ==";
+
+        private CloudQueue _queue;
+
+        public CloudQueueService()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            _queue = queueClient.GetQueueReference(QueueName);
+        }
+
+        public async Task CreateNewQueueIfNotExistsAsync()
+        {
+            bool createdQueue = await _queue.CreateIfNotExistsAsync();
+            if (createdQueue)
+            {
+                Console.WriteLine("The queue of news articles was created.");
+            }
+        }
+
+        public async Task SendArticleAsync(string newsMessage)
+        {
+            CloudQueueMessage articleMassage = new CloudQueueMessage(newsMessage);
+            await _queue.AddMessageAsync(articleMassage);
+        }
+
+        public async Task<string> ReceiveArticleAsync()
+        {
+            bool exists = await _queue.ExistsAsync();
+            if (!exists)
+                throw new Exception($"Queue with name {QueueName} not exists");
+
+            CloudQueueMessage message = await _queue.GetMessageAsync();
+            if (message != null)
+            {
+                string messageText = message.AsString;
+                await _queue.DeleteMessageAsync(message);
+                return messageText;
+            }
+
+            return string.Empty;
+        }
+    }
+}
---
+++ azure/AzureQueueStorage/QueueApp/QueueApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using QueueApp.Core;
+
+namespace QueueApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cloudQueueService = new CloudQueueService();
+
+                await cloudQueueService.CreateNewQueueIfNotExistsAsync();
+                
+                if (args.Length > 0)
+                {
+                    var value = string.Join(" ", args);
+
+                    await cloudQueueService.SendArticleAsync(value);
+
+                    Console.WriteLine($"Sent {value}");
+
+                }
+                else
+                {
+                    var result = await cloudQueueService.ReceiveArticleAsync();
+                    Console.WriteLine($"Result: {result}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
