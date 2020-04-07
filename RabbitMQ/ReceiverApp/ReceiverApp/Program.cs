a2207803512e4f532d55457f08a8d07e7ea53b63
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-04-08 00:43:30 +0200

    Add example of working with rabbitmq

---
+++ RabbitMQ/ReceiverApp/ReceiverApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiverApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Consumer_Received;
+
+                    while (true)
+                    {
+                        Console.WriteLine("Checkinh queue...");
+
+                        channel.BasicConsume(queue: "hello",
+                        autoAck: true,
+                        consumer: consumer);
+
+                        Thread.Sleep(5000);
+                    }
+                }
+
+                
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadKey();
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body;
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($" [x] Received {message}");
+        }
+    }
+}
---
+++ RabbitMQ/SenderApp/SenderApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace SenderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+                var index = 1;
+
+                while (true)
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(queue: "hello",
+                            durable: false,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null);
+
+                        while (true)
+                        {
+                            string message = $"Hello World! #{index++}";
+                            var body = Encoding.UTF8.GetBytes(message);
+
+                            Console.WriteLine($"Sending {index} message...");
+
+                            channel.BasicPublish(exchange: "",
+                            routingKey: "hello",
+                            basicProperties: null,
+                            body: body);
+
+                            Console.WriteLine($" [x] Sent {message}", message);
+
+                            Thread.Sleep(3000);
+                        }
+                    }
+                }
+
+                
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadKey();
+        }
+    }
+}
