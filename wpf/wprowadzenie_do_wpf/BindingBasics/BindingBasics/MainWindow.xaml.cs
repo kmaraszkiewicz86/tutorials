8d87dd9d0d32f2ca6976daa816845ebf43f1f45e
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-05-08 17:18:39 +0200

    Add basics of wpf bindings wpf tutorial

---
+++ wpf/wprowadzenie_do_wpf/BindingBasics/BindingBasics/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace YourAppName
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Produkt p1 = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateDataContext();
+        }
+
+        private void CreateDataContext()
+        {
+            p1 = new Produkt("DZ-10", "spasiona kurwiej", 150, "Wscielej dupy");
+            GridOfProduct.DataContext = p1;
+
+        }
+
+        private void btnPotwierdz_Click(object sender, RoutedEventArgs e)
+        {
+            string tekst = String.Format("{0}{1}{2}", "Wprowadzono dane:", Environment.NewLine, p1.ToString()); MessageBox.Show(tekst);
+        }
+    }
+}
