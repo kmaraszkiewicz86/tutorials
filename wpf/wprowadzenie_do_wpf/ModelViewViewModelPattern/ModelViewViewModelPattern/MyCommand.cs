145f9348eed26aef7081ceeaf3bddda56e0c3969
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-05-09 23:58:25 +0200

    Add mvvm model example for wpf tutorial

---
+++ wpf/wprowadzenie_do_wpf/ModelViewViewModelPattern/ModelViewViewModelPattern/MyCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace ModelViewViewModelPattern
+{
+    public class MyCommand : ICommand
+    {
+        Action _execute;
+        public MyCommand(Action executeMethod)
+        {
+            _execute = executeMethod;
+        }
+        public void Execute(object parameter)
+        {
+            if (_execute != null)
+            {
+                _execute();
+            }
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public event EventHandler CanExecuteChanged;
+    }
+}
---
+++ wpf/wprowadzenie_do_wpf/ModelViewViewModelPattern/ModelViewViewModelPattern/Student.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ModelViewViewModelPattern.Annotations;
+
+namespace ModelViewViewModelPattern
+{
+    public class Student: INotifyPropertyChanged
+    {
+        private string name;
+
+        private string lastname;
+
+        private int _startYearAtUniversity;
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (name != value)
+                {
+                    name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public string Lastname
+        {
+            get => lastname;
+            set
+            {
+                if (lastname != value)
+                {
+                    lastname = value;
+                    OnPropertyChanged(nameof(Lastname));
+                }
+            }
+        }
+
+        public int StartYearAtUniversity
+        {
+            get => _startYearAtUniversity;
+            set
+            {
+                if (_startYearAtUniversity != value)
+                {
+                    _startYearAtUniversity = value;
+                    OnPropertyChanged(nameof(StartYearAtUniversity));
+                }
+            }
+        }
+
+        public string TimeInUniversity =>
+            (DateTime.Now.Year - StartYearAtUniversity).ToString();
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
