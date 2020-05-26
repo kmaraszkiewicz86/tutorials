53960915a3f3d1a474477ee11224aa6753f0b9ec
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-05-26 23:10:51 +0200

    Add grpc service implemnetations and client basic impl

---
+++ gRPC/Tutorial/CreditRatingService/Service/CreditRatingClient/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CreditRatingClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class Result
+        {
+            public string Test { get; set; } = "Brak danych";
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DoWork();
+        }
+
+        private void DoWork()
+        {
+            var testInstance = new Result()
+            {
+                Test = "TEstowow"
+            };
+
+            DataContext = testInstance;
+        }
+    }
+}
---
+++ gRPC/Tutorial/CreditRatingService/Service/CreditRatingService/Services/CreditRatingService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CreditRatingService.Protos;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace CreditRatingService.Services
+{
+    public class CreditRatingService: CreditRatingCheck.CreditRatingCheckBase
+    {
+        private readonly ILogger<CreditRatingService> _logger;
+        private static readonly Dictionary<string, Int32> customerTrustedCredit = new Dictionary<string, Int32>()
+        {
+            {"id0201", 10000},
+            {"id0417", 5000},
+            {"id0306", 15000}
+        };
+
+        public CreditRatingService(ILogger<CreditRatingService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CreditReply> CheckCreditRequest(CreditRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new CreditReply
+            {
+                IsAcepted = IsEligibleForCredit(request.CustomerId, request.Credit)
+            });
+        }
+
+        private bool IsEligibleForCredit(string customerId, Int32 credit)
+        {
+            bool isEligible = false;
+
+            if (customerTrustedCredit.TryGetValue(customerId, out Int32 maxCredit))
+            {
+                isEligible = credit <= maxCredit;
+            }
+
+            return isEligible;
+        }
+    }
+}
