1a3cf57c1de6e3f6967976a9d3ce15fa7b4f4f7a
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-05-30 21:29:21 +0200

    Add kafka tutorial example

---
+++ kafka/KafkaConsumer/KafkaConsumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "test-consumers",
+                BootstrapServers = "localhost:9092",
+                EnableAutoCommit = false
+            };
+
+            using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+            {
+                consumer.Subscribe("test-topic");
+
+                while(true)
+                {
+                    try
+                    {
+                        var result = consumer.Consume();
+
+                        Console.WriteLine($"Message => {result.Message.Value}; at: {result.TopicPartitionOffset}");
+
+                        Thread.Sleep(2000);
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occoured => {e.Error.Reason}");
+                    }                    
+                }
+            }
+        }
+    }
+}
---
+++ kafka/KafkaProducer/KafkaProducer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Confluent.Kafka;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            Action<DeliveryReport<Null, string>> handler = r =>
+                Console.WriteLine(!r.Error.IsError
+                ? $"Delivered messege to {r.TopicPartitionOffset}"
+                : $"Delivered Error: {r.Error.Reason}");
+
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                var stringValue = string.Empty;
+
+                for (int i = 0; i < 20; i++)
+                {
+                    stringValue += i.ToString() + "";
+
+                    producer.ProduceAsync("test-topic", new Message<Null, string> { Value = stringValue });
+
+                    producer.Flush(TimeSpan.FromSeconds(2));
+                }
+            }
+
+
+        }
+    }
+}
