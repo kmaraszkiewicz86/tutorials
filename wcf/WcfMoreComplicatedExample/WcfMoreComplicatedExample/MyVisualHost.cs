117b707efb72e4952eadedad9ba9376be9830356
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-05-31 23:00:48 +0200

    Add wcf tutorial app

---
+++ wcf/WcfMoreComplicatedExample/WcfMoreComplicatedExample/MyVisualHost.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WcfMoreComplicatedExample
+{
+    public class MyVisualHost : FrameworkElement
+    {
+        private readonly VisualCollection _children;
+
+        protected override int VisualChildrenCount => _children.Count;
+
+        protected override Visual GetVisualChild(int index)
+        {
+            if (index < 0 || index >= _children.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return _children[index];
+        }
+
+        public MyVisualHost()
+        {
+            _children = new VisualCollection(this)
+            {
+                new SimpleVisual()
+            };
+        }
+    }
+}
---
+++ wcf/WcfMoreComplicatedExample/WcfMoreComplicatedExample/SimpleVisual.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WcfMoreComplicatedExample
+{
+    public class SimpleVisual: DrawingVisual
+    {
+        public SimpleVisual()
+        {
+            StreamGeometry geometry = new StreamGeometry();
+            using (var geometryContext = geometry.Open())
+            {
+                geometryContext.BeginFigure(new Point(0, 0),
+                    true, true);
+                geometryContext.LineTo(new Point(100, 0),
+                    true, true);
+                geometryContext.ArcTo(new Point(0, 100),
+                    new Size(100, 100), 0.0, false, SweepDirection.Clockwise,
+                    true, true);
+                geometryContext.LineTo(new Point(0, 0), false, false);
+
+                using (DrawingContext context = RenderOpen())
+                {
+                    var pen = new Pen(Brushes.Red, 2.0);
+                    context.DrawGeometry(Brushes.Red, pen, geometry);
+                }
+            }
+        }
+    }
+}
