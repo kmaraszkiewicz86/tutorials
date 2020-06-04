637df17e5a756aad50096287abcb82807b1772b8
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-06-04 21:50:30 +0200

    Add mvvm example for xamarin tutorial

---
+++ xamarin/StackLayoutTutorial/StackLayoutTutorial/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace StackLayoutTutorial
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private int Count = 0;
+
+        private string _testString = "Empty Label";
+
+        public string TestString
+        {
+            get => _testString;
+            set
+            {
+                _testString = value;
+                OnPropertyChanged(nameof(TestString));
+            }
+        }
+
+        private bool _canExecute;
+
+        public bool CanExecute
+        {
+            get => _canExecute;
+            set
+            {
+                _canExecute = value;
+                OnPropertyChanged(nameof(CanExecute));
+            }
+        }
+
+        public ICommand SayHelloCommand { get; }
+
+        public ViewModel()
+        {
+            CanExecute = true;
+
+            SayHelloCommand = new Command(() =>
+            {
+                TestString = $"Clicked {(++Count)} times";
+                if (Count == 10)
+                {
+                    CanExecute = false;
+                }
+            },
+            () =>
+            {
+                return CanExecute;
+
+            });
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
