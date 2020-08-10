23b3d0e05afba23098d93c71d569a06526e1249b
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-08-10 21:19:02 +0200

    Update blazor tutorial app

--- dotnet/blazor/BlazorAppTutorial/BlazorAppTutorial/Pages/EmployeeOverviewBase.cs
+++ dotnet/blazor/BlazorAppTutorial/BlazorAppTutorial/Pages/EmployeeOverviewBase.cs
@@ -27,33 +27,33 @@ namespace BlazorAppTutorial.Pages
         private void InitializeJobCategories()
 		{
 			JobCategories = new List<JobCategory>()
-	{
-		new JobCategory{JobCategoryId = 1, JobCategoryName = "Pie research"},
-		new JobCategory{JobCategoryId = 2, JobCategoryName = "Sales"},
-		new JobCategory{JobCategoryId = 3, JobCategoryName = "Management"},
-		new JobCategory{JobCategoryId = 4, JobCategoryName = "Store staff"},
-		new JobCategory{JobCategoryId = 5, JobCategoryName = "Finance"},
-		new JobCategory{JobCategoryId = 6, JobCategoryName = "QA"},
-		new JobCategory{JobCategoryId = 7, JobCategoryName = "IT"},
-		new JobCategory{JobCategoryId = 8, JobCategoryName = "Cleaning"},
-		new JobCategory{JobCategoryId = 9, JobCategoryName = "Bakery"},
-		new JobCategory{JobCategoryId = 9, JobCategoryName = "Bakery"}
-	};
+			{
+				new JobCategory{JobCategoryId = 1, JobCategoryName = "Pie research"},
+				new JobCategory{JobCategoryId = 2, JobCategoryName = "Sales"},
+				new JobCategory{JobCategoryId = 3, JobCategoryName = "Management"},
+				new JobCategory{JobCategoryId = 4, JobCategoryName = "Store staff"},
+				new JobCategory{JobCategoryId = 5, JobCategoryName = "Finance"},
+				new JobCategory{JobCategoryId = 6, JobCategoryName = "QA"},
+				new JobCategory{JobCategoryId = 7, JobCategoryName = "IT"},
+				new JobCategory{JobCategoryId = 8, JobCategoryName = "Cleaning"},
+				new JobCategory{JobCategoryId = 9, JobCategoryName = "Bakery"},
+				new JobCategory{JobCategoryId = 9, JobCategoryName = "Bakery"}
+			};
 		}
 
 		private void InitializeCountries()
 		{
 			Countries = new List<Country>
-	{
-		new Country {CountryId = 1, Name = "Belgium"},
-		new Country {CountryId = 2, Name = "Netherlands"},
-		new Country {CountryId = 3, Name = "USA"},
-		new Country {CountryId = 4, Name = "Japan"},
-		new Country {CountryId = 5, Name = "China"},
-		new Country {CountryId = 6, Name = "UK"},
-		new Country {CountryId = 7, Name = "France"},
-		new Country {CountryId = 8, Name = "Brazil"}
-	};
+			{
+				new Country {CountryId = 1, Name = "Belgium"},
+				new Country {CountryId = 2, Name = "Netherlands"},
+				new Country {CountryId = 3, Name = "USA"},
+				new Country {CountryId = 4, Name = "Japan"},
+				new Country {CountryId = 5, Name = "China"},
+				new Country {CountryId = 6, Name = "UK"},
+				new Country {CountryId = 7, Name = "France"},
+				new Country {CountryId = 8, Name = "Brazil"}
+			};
 		}
 
 		private void InitializeEmployees()
@@ -101,5 +101,11 @@ namespace BlazorAppTutorial.Pages
 			};
 			Employees = new List<Employee> { e1, e2 };
 		}
+
+		public void ClickTest()
+        {
+			Employees.First().FirstName = "Chujek";
+			Employees.First().LastName = "chujek";
+        }
 	}
 }
