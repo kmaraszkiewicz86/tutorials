505c8ae0aa25e7c5915ccd260d78ae27e444e20d
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-09-22 20:56:00 +0200

    Add tutorial entity frameowrk core project

---
+++ dotnet/EntityFrameworkTutorial/RelationshipTesting/EntityFrameworkRelationshipsTesting/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using EntityFrameworkRelationshipsTesting.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace EntityFrameworkRelationshipsTesting
+{
+    public class AppDbContextDesign : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(
+                "Server=WINDOWSIZABELAM;Database=EntityFrameworkRelationshipsTesting;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Dog> Dogs { get; set; }
+
+        public DbSet<AdditionalName> AdditionalNames { get; set; }
+
+        public DbSet<Puppy> Puppies { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ dotnet/EntityFrameworkTutorial/RelationshipTesting/EntityFrameworkRelationshipsTesting/Entities/Dog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkRelationshipsTesting.Entities
+{
+    public class Dog
+    {
+        public int DogId { get; set; }
+
+        public string Name { get; set; }
+
+        public AdditionalName AdditionalName { get; set; }
+
+        public ICollection<Puppy> Puppies { get; set; }
+    }
+
+    public class AdditionalName
+    {
+        public int AdditionalNameId { get; set; }
+
+        public string Name { get; set; }
+
+        public int DogId { get; set; }
+    }
+
+    public class Puppy
+    {
+        public int PuppyId { get; set; }
+
+        public string Name { get; set; }
+
+        public Dog Dog { get; set; }
+    }
+}
