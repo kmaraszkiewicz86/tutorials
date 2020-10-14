b950b630f9b24b64301c91780c385b53d947b495
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2020-10-14 22:14:53 +0200

    Add nativeElement and debugElement unit tests exapmle

--- angular/unit_testing/PSAngularUnitTestingCourse/src/app/hero/hero.component.shallow.spec.ts
+++ angular/unit_testing/PSAngularUnitTestingCourse/src/app/hero/hero.component.shallow.spec.ts
@@ -1,5 +1,6 @@
 import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { By } from "@angular/platform-browser";
 import { HeroComponent } from "./hero.component"
 
 describe('HeroComponent (shallow test)', () => {
@@ -20,5 +21,16 @@ describe('HeroComponent (shallow test)', () => {
 
         expect(fixture.componentInstance.hero.name).toEqual('hero1');
     })
+
+    it('should render hero name in the anchor tag', () => {
+        fixture.componentInstance.hero = { id: 1, name: 'hero1', strength: 3 };
+        fixture.detectChanges();
+
+        let debugElementOfAnchor = fixture.debugElement.query(By.css('a'))
+
+        expect(debugElementOfAnchor.nativeElement.textContent).toContain('hero1')
+
+        expect(fixture.nativeElement.querySelector('a').textContent).toContain('hero1')
+    });
 
 })
