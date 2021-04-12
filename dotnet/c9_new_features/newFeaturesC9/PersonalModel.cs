50b150552a5f2cd29de560ef1fe6cd8be3a6b296
kmaraszkiewicz86 <50966276+kmaraszkiewicz86@users.noreply.github.com>
2021-04-13 00:07:27 +0200

    Added new features of the csharp 9 examples

---
+++ dotnet/c9_new_features/newFeaturesC9/PersonalModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace newFeaturesC9
+{
+    public class PersonalModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public PersonalModel()
+        {
+        }
+
+        public PersonalModel(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
